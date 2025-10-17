49adfac69c3e266d65c0d7166160891c9b0b64c2
Muhammad Rizky Firdaus <rizky.firdaus@willowmore.com.sg>
2025-10-17 20:49:08 +0700

    adjust file

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import { Type, Square, Image as ImageIcon } from "lucide-react";
 import type { FabricDesign } from "../interface/FabricDesignInterface";
 
